08db461a6db568177412e03bc84f98828465dc2f
Chris Decelles <cdecelles@pivotal.io>
2015-10-12 15:20:01 -0400

    Remove initial code.

--- labs/initial/guestbook/src/main/java/io/spring/cloud/samples/guestbook/repositories/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package io.spring.cloud.samples.guestbook.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.spring.cloud.samples.guestbook.domain.Message;
-
-public interface MessageRepository extends CrudRepository<Message, Long> {
-
-}
