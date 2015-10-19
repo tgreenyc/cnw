5b1aa404f202c15597c21228f11805fd4d28f492
Nishtha Agarwal <nishtha2479@gmail.com>
2015-10-19 18:27:17 -0400

    Updated the initial labs folder

--- labs/initial/guestbook/src/main/java/io/spring/cloud/samples/guestbook/repositories/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package io.spring.cloud.samples.guestbook.repositories;
-
-import io.spring.cloud.samples.guestbook.domain.Message;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<Message, Long> {
-
-}
