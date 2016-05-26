94f7502062bd466224536124b877233a184a7a0e
jaimegag <jaguilar@pivotal.io>
2016-05-25 20:09:33 -0400

    Fix starter and remove cloud dependencies for guestbook

--- labs/initial/guestbook/src/main/java/io/spring/cloud/samples/guestbook/GuestbookApplication.java
+++ labs/initial/guestbook/src/main/java/io/spring/cloud/samples/guestbook/GuestbookApplication.java
@@ -1,10 +1,7 @@
 package io.spring.cloud.samples.guestbook;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class GuestbookApplication {
